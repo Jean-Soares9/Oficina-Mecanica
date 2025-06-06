d45cf25b0694bb1d13975be6f4a6ad4ec3c92012
JEAN ANDRADE SOARES <jean.asoares@senacsp.edu.br>
2025-06-05 22:00:49 -0300

    Cadastro de cliente no mongoDB

--- src/views/rendererCliente.js
+++ src/views/rendererCliente.js
@@ -48,3 +48,62 @@ let cityClient = document.getElementById('inputCityClient')
 let ufClient = document.getElementById('inputUFClient')
 // Uso do ID para o delete e update
 let idClient = document.getElementById('inputIdClient')
+
+// ============================================================
+// == CRUD Create/Update ======================================
+
+//Evento associado ao botão submit (uso das validações do html)
+frmClient.addEventListener('submit', async (event) => {
+    //evitar o comportamento padrão do submit que é enviar os dados do formulário e reiniciar o documento html
+    event.preventDefault()
+    // Teste importante (recebimento dos dados do formuláro - passo 1 do fluxo)
+    //console.log(nameClient.value, cpfClient.value, emailClient.value, phoneClient.value, cepClient.value, addressClient.value, numberClient.value, complementClient.value, neighborhoodClient.value, cityClient.value, ufClient.value)
+    //Criar um objeto para armazenar os dados do cliente antes de enviar ao main
+
+    // Estratégia para usar o submit para cadastrar um novo Cliente ou editar os dados de um Cliente ja existe
+    // verificar se existe o id do cliente
+    if (idClient.value === "") {
+        // cadastrar um novo cliente
+        const client = {
+            nameCli: nameClient.value,
+            cpfCli: cpfClient.value,
+            emailCli: emailClient.value,
+            phoneCli: phoneClient.value,
+            cepCli: cepClient.value,
+            addressCli: addressClient.value,
+            numberCli: numberClient.value,
+            complementCli: complementClient.value,
+            neighborhoodCli: neighborhoodClient.value,
+            cityCli: cityClient.value,
+            ufCli: ufClient.value
+        }
+        // Enviar ao main o objeto client - (Passo 2: )
+        // uso do preload.js
+        api.newClient(client)
+    } else {
+        // alterar os dados de um cliente existente
+        // teste de validação do id
+        // console.log(idClient.value)
+        // editar um cliente existente
+        const client = {
+            idCli: idClient.value,
+            nameCli: nameClient.value,
+            cpfCli: cpfClient.value,
+            emailCli: emailClient.value,
+            phoneCli: phoneClient.value,
+            cepCli: cepClient.value,
+            addressCli: addressClient.value,
+            numberCli: numberClient.value,
+            complementCli: complementClient.value,
+            neighborhoodCli: neighborhoodClient.value,
+            cityCli: cityClient.value,
+            ufCli: ufClient.value
+        }
+        // enviar ao main o objeto client - (passo 2: fluxo)
+        // uso preload.js
+        api.updateClient(client)
+    }
+});
+
+// == Fim CRUD Create/Update ==================================
+// ============================================================
